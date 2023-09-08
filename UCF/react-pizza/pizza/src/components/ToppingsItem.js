bea95ebb0d73f5ce0d102f37110f7ec071302ac4
jahnathan x <jahnathanexantus@gmail.com>
2023-09-07 22:44:13 -0400

    created the functionality for the menu page

--- UCF/react-pizza/pizza/src/components/ToppingsItem.js
+++ UCF/react-pizza/pizza/src/components/ToppingsItem.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { toppings } from '../options';
 
+const ToppingsItem = ({ name, price, addToCart }) => {
+  const handleAddToCart = () => {
+    addToCart(name, price);
+  };
 
-const toppingsItem = ({name,price},index) => {
   return (
-    <div className="stats__box" key={index}>
-        <h3 className="stats__no">{name}</h3>
-        <p className="stats__title">{price}</p>
-      </div>
+    <div>
+      <div className='toppings__name'>{name}</div>
+      <p>{price}</p>
+      <button onClick={handleAddToCart}>Choose</button>
+    </div>
   );
-}
+};
 
-export default toppingsItem;
+export default ToppingsItem;
--- UCF/react-pizza/pizza/src/pages/menu/Menu.js
+++ UCF/react-pizza/pizza/src/pages/menu/Menu.js
@@ -1,23 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { toppings } from "../../options";
-import ToppingsItem from '../../components/ToppingsItem.js'
+import ToppingsItem from "../../components/ToppingsItem.js";
 
 const Menu = () => {
-  return (
-   <div>
-    <h1 className="section__title">You <span>Pick</span></h1>
+    const [cart, setCart] = useState([]);
+    const addToCart = (name, price) => {
+    const newItem = { name, price };
+    setCart([...cart, newItem]);
+    };
 
+    return (
+    <div>
+        <h1 className="section__title">
+        You <span>Pick</span>
+        </h1>
 
-    <div className="toppin__cart-container">
+        <div className="toppin__cart-container">
         <div className="toppings__container">
-            {toppings.map((item)=>{
-                return <ToppingsItem key={item.id} {...item}/>
-            })}
+            {toppings.map((item) => (
+            <ToppingsItem key={item.id} {...item} addToCart={addToCart} />
+            ))}
         </div>
-        <div className="cart__container"></div>
+
+        <div className="cart__container">
+            <h2>Cart</h2>
+            <ul>
+            {cart.map((item, index) => (
+                <li key={index}>
+                {item.name} - ${item.price}
+                </li>
+            ))}
+            </ul>
+        </div>
+        </div>
     </div>
-   </div>
-  );
+    );
 };
 
 export default Menu;
